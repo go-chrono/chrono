c7c7ca3ada0fed693525b175e48f3402efdd873f
joe-mann <contact@joemann.co.uk>
2022-03-02 16:55:35 +0100

    Add tests for Instant.Compare.

--- instant_test.go
+++ instant_test.go
@@ -3,6 +3,7 @@ package chrono_test
 import (
 	"testing"
 
+	"github.com/go-chrono/chrono"
 	chronotest "github.com/go-chrono/chrono/test"
 )
 
@@ -10,6 +11,25 @@ func TestInstant_String(t *testing.T) {
 	i := chronotest.InstantOf(11e14)
 	if str := i.String(); str != "1100000000000000" {
 		t.Fatalf("i.String() = %s, want 1100000000000000", str)
+	}
+}
+
+func TestInstant_Compare(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		i        chrono.Instant
+		i2       chrono.Instant
+		expected int
+	}{
+		{"earlier", chronotest.InstantOf(11e14), chronotest.InstantOf(12e14), -1},
+		{"later", chronotest.InstantOf(12e14), chronotest.InstantOf(11e14), 1},
+		{"equal", chronotest.InstantOf(11e14), chronotest.InstantOf(11e14), 0},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			if v := tt.i.Compare(tt.i2); v != tt.expected {
+				t.Errorf("i.Compare(i2) = %d, want %d", v, tt.expected)
+			}
+		})
 	}
 }
 
