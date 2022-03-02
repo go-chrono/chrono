96866cf3b0db40b266835b1f9d5933d37bc0269a
joe-mann <contact@joemann.co.uk>
2022-03-02 16:54:37 +0100

    Implement Instant.Compare.

--- instant.go
+++ instant.go
@@ -15,6 +15,23 @@ func Now() Instant {
 	}
 }
 
+// Compare compares i with i2. If i is before i2, it returns -1;
+// if i is after i2, it returns 1; if they're the same, it returns 0.
+func (i Instant) Compare(i2 Instant) int {
+	switch {
+	case i.v == nil:
+		panic("i is not initialized")
+	case i2.v == nil:
+		panic("i2 is not initialized")
+	case *i.v < *i2.v:
+		return -1
+	case *i.v > *i2.v:
+		return 1
+	default:
+		return 0
+	}
+}
+
 // Elapsed is shorthand for i.Until(chrono.Now()).
 func (i Instant) Elapsed() Duration {
 	return i.Until(Now())
