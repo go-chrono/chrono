4285e9629deb494ca260784c5aaace06b1c147d3
joe-mann <contact@joemann.co.uk>
2022-04-02 18:25:21 +0200

    Test validity of specifiers in format layout in different contexts.

--- format_test.go
+++ format_test.go
@@ -6,6 +6,116 @@ import (
 	"github.com/go-chrono/chrono"
 )
 
+const (
+	year  = 2020
+	month = chrono.March
+	day   = 13
+	hour  = 12
+	min   = 30
+	sec   = 15
+	nsec  = 0
+)
+
+var (
+	dateSpecifiers = []struct {
+		specifier string
+		expected  string
+	}{
+		{"%Y", "2020"},
+		{"%EY", "2020"},
+		{"%y", "20"},
+		{"%Ey", "20"},
+		{"%j", "073"},
+		{"%m", "03"},
+		{"%B", "March"},
+		{"%b", "Mar"},
+		{"%d", "13"},
+		{"%u", "5"},
+		{"%A", "Friday"},
+		{"%a", "Fri"},
+		{"%G", "2020"},
+		{"%V", "11"},
+	}
+
+	timeSpecifiers = []struct {
+		specifier string
+		expected  string
+	}{
+		{"%P", "pm"},
+		{"%p", "PM"},
+		{"%I", "12"},
+		{"%H", "12"},
+		{"%M", "30"},
+		{"%S", "15"},
+	}
+)
+
+func TestLocalDate_Format_supported_specifiers(t *testing.T) {
+	for _, tt := range dateSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			if formatted := chrono.LocalDateOf(year, month, day).Format(tt.specifier); formatted != tt.expected {
+				t.Errorf("date.Format(%s) = %s, want %s", tt.specifier, formatted, tt.expected)
+			}
+		})
+	}
+
+	for _, tt := range timeSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			func() {
+				defer func() {
+					if r := recover(); r == nil {
+						t.Error("expecting panic that didn't occur")
+					}
+				}()
+
+				chrono.LocalDateOf(year, month, day).Format(tt.specifier)
+			}()
+		})
+	}
+}
+
+func TestLocalTime_Format_supported_specifiers(t *testing.T) {
+	for _, tt := range dateSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			func() {
+				defer func() {
+					if r := recover(); r == nil {
+						t.Error("expecting panic that didn't occur")
+					}
+				}()
+
+				chrono.LocalTimeOf(hour, min, sec, nsec).Format(tt.specifier)
+			}()
+		})
+	}
+
+	for _, tt := range timeSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			if formatted := chrono.LocalTimeOf(hour, min, sec, nsec).Format(tt.specifier); formatted != tt.expected {
+				t.Errorf("time.Format(%s) = %s, want %s", tt.specifier, formatted, tt.expected)
+			}
+		})
+	}
+}
+
+func TestLocalDateTime_Format_supported_specifiers(t *testing.T) {
+	for _, tt := range dateSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			if formatted := chrono.LocalDateTimeOf(year, month, day, hour, min, sec, nsec).Format(tt.specifier); formatted != tt.expected {
+				t.Errorf("datetime.Format(%s) = %s, want %s", tt.specifier, formatted, tt.expected)
+			}
+		})
+	}
+
+	for _, tt := range timeSpecifiers {
+		t.Run(tt.specifier, func(t *testing.T) {
+			if formatted := chrono.LocalDateTimeOf(year, month, day, hour, min, sec, nsec).Format(tt.specifier); formatted != tt.expected {
+				t.Errorf("datetime.Format(%s) = %s, want %s", tt.specifier, formatted, tt.expected)
+			}
+		})
+	}
+}
+
 func TestLocalDateTime_Format_literals(t *testing.T) {
 	for _, tt := range []struct {
 		name     string
